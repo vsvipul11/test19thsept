36489ba6a865f76e85a7cfc5c317ab4c79a3c1f3
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2024-09-19 16:01:53 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
 // next.config.mjs
 export const nextConfig = {
-  // Remove the output: 'export' configuration
-  // Set custom directory for build output
-  distDir: 'build',
+  // Remove the custom distDir if you want to use the default '.next' directory
+  // distDir: 'build',
   
-  // TypeScript configuration
   typescript: {
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
     ignoreBuildErrors: true,
   },
 };
 
-// Export the configuration object directly
 export default nextConfig;
